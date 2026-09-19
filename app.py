5a4eb8db9c61946f270e07629e183e7b9804e0fa
chanthiralega2008-dev <chanthiralega2008@gmail.com>
2026-09-19 12:15:26 +0530

    Convert Python app to Flask

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello, GitHub! This is my first Python application.")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, GitHub! This is my first Python application."
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=10000)
